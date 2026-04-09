f600228bb8913a885e575c1afd064df11ad131cf
Victor LI <victor-li@outlook.com>
2026-04-09 13:49:05 +0800

    chore: remove all custom keybindings, defer to plugin defaults
    - Remove format, diagnostic, and git hunk navigation keymaps from init.lua
    - Remove gitsigns on_attach hunk keymaps
    - Remove telescope leader/Ctrl-P keymaps and insert-mode mapping overrides
    - Add copilot.vim to lazy-lock.json
    - Update LuaSnip and telescope.nvim lockfile commits

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -39,30 +39,7 @@ return {
         row = 0,
         col = 1
       },
-      on_attach = function(bufnr)
-        local gs = package.loaded.gitsigns
 
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        -- Stage/unstage hunks
-        map("n", "<leader>hs", gs.stage_hunk, { desc = "Stage hunk" })
-        map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "Undo stage hunk" })
-        map("n", "<leader>hr", gs.reset_hunk, { desc = "Reset hunk" })
-        map("v", "<leader>hs", function() gs.stage_hunk({vim.fn.line("."), vim.fn.line("v")}) end, { desc = "Stage selected lines" })
-        map("v", "<leader>hr", function() gs.reset_hunk({vim.fn.line("."), vim.fn.line("v")}) end, { desc = "Reset selected lines" })
-        
-        -- Stage/reset buffer
-        map("n", "<leader>hS", gs.stage_buffer, { desc = "Stage buffer" })
-        map("n", "<leader>hR", gs.reset_buffer, { desc = "Reset buffer" })
-        
-        -- Preview and blame
-        map("n", "<leader>hp", gs.preview_hunk, { desc = "Preview hunk" })
-        map("n", "<leader>hb", function() gs.blame_line({full=true}) end, { desc = "Blame line" })
-      end,
     })
   end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,19 +7,9 @@ return {
   },
   config = function()
     local telescope = require("telescope")
-    local actions = require("telescope.actions")
 
     telescope.setup({
-      defaults = {
-        mappings = {
-          i = {
-            ["<C-j>"] = actions.move_selection_next,
-            ["<C-k>"] = actions.move_selection_previous,
-            ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-            ["<esc>"] = actions.close,
-          },
-        },
-      },
+      defaults = {},
       pickers = {
         find_files = {
           hidden = true,
@@ -39,19 +29,5 @@ return {
     -- Load fzf extension
     telescope.load_extension("fzf")
 
-    -- Keymaps
-    local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
-    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
-    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
-    vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Document symbols" })
-    vim.keymap.set("n", "<leader>fw", builtin.lsp_workspace_symbols, { desc = "Workspace symbols" })
-    vim.keymap.set("n", "<leader>fr", builtin.lsp_references, { desc = "LSP references" })
-    vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Diagnostics" })
-    vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Recent files" })
-    vim.keymap.set("n", "<leader>fc", builtin.git_commits, { desc = "Git commits" })
-    vim.keymap.set("n", "<leader>fgc", builtin.git_bcommits, { desc = "Buffer git commits" })
-    vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find files" })
   end,
 }
